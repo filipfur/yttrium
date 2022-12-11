a9be68a115b7c5258a50b86c263a7c41ef8b9400
filipfur <filipfur@live.se>
2022-12-11 20:28:11 +0100

    loading objects (prefabs) pos, uv, normals, loading image textures

--- smoothfile.py
+++ smoothfile.py
@@ -18,6 +18,77 @@ def _translatecpp():
 def _translatets():
     return modeltosrc.translatets(PATH_MODELS_CORE, PATH_GEN, "core")
 
+def _mesh(filepath, uvx=1):
+    file = open(filepath, 'r')
+    lines = file.readlines()
+    vertices = []
+    uvs = []
+    normals = []
+    indices = []
+    outVertices = []
+    outIndices = []
+
+    for line in lines:
+        if line.startswith("v "):
+            line = line.strip()
+            arr = line.split()
+            arr = arr[1:]
+            vertices.append(arr)
+        elif line.startswith("vt "):
+            line = line.strip()
+            arr = line.split()
+            arr = arr[1:]
+            uvs.append([str(float(x) * float(uvx)) for x in arr])
+        elif line.startswith("vn "):
+            line = line.strip()
+            arr = line.split()
+            arr = arr[1:]
+            normals.append(arr)
+        elif line.startswith("f "):
+            line = line.strip()
+            arr = line.split()
+            arr = arr[1:]
+            for x in arr:
+                indices.append([int(x) for x in x.split("/")])
+            outIndices.append(len(outIndices))
+
+    #print("len(vertices) :" + str(len(vertices)))
+
+    for i in range(len(indices)):
+        #print(f"index={index}")
+        outVertices.append(vertices[indices[i][0] - 1]) # Positions
+        outVertices.append(uvs[indices[i][1] - 1]) # UVs
+        outVertices.append(normals[indices[i][2] - 1]) # Normals
+
+
+    #indices.append(len(indices))
+    name = os.path.basename(filepath)
+    name = name[0:name.find(".")]
+
+    content = ['import { VertexArray } from "../gen/core/vertexarray"',
+        f"export class Asset_{name} extends VertexArray",
+        '{',
+        '    constructor() {',
+        '        super([3, 2, 3],',
+        '            [' + ", ".join([", ".join(x) for x in outVertices]) + '],',
+        '            [' + ", ".join([str(x) for x in []]) + ']);',
+        '    }',
+        '}']
+
+
+    outstr = "\n".join(content)
+    #print(outstr)
+
+    assetDir = os.path.join("src", "assets")
+    if(not os.path.exists(assetDir)):
+        os.makedirs(assetDir)
+
+    with open(os.path.join(assetDir, name + ".ts"), "w") as f:
+        f.write(outstr)
+
+    return True
+
+
 def _gen():
     if _reflect():
         return _translatets()
--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,10 @@ confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
 })({ particleCount: 200, spread: 200 });*/
 
 //import './webglcontext'
+import { Asset_block } from "./assets/block";
+import { Asset_plane } from "./assets/plane";
+import { Asset_rabbit } from "./assets/rabbit";
+import { ImageTexture } from "./gen/core/imagetexture";
 import { ShaderProgram } from "./gen/core/shaderprogram"
 import { VertexArray } from "./gen/core/vertexarray";
 
@@ -80,12 +84,20 @@ const cubeIndices = [
 const cubeVert = `#version 300 es
 precision highp float;
 layout (location=0) in vec3 aPos;
+layout (location=1) in vec2 aTexCoord;
+layout (location=2) in vec3 aNormal;
 out vec3 FragPos;
+out vec2 TexCoord;
+out vec3 Normal;
 uniform mat4 u_model;
 uniform mat4 u_projection;
 uniform mat4 u_view;
 void main() {
   FragPos = vec3(u_model * vec4(aPos.xyx, 1.0));
+  TexCoord = aTexCoord;
+  TexCoord.y = 1.0 - TexCoord.y;
+  mat3 normalMatrix = transpose(inverse(mat3(u_model)));
+  Normal = normalize(normalMatrix * aNormal);
   gl_Position = u_projection * u_view * u_model * vec4(aPos, 1.0);
 }
 `;
@@ -93,14 +105,31 @@ void main() {
 const cubeFrag = `#version 300 es
 precision highp float;
 in vec3 FragPos;
+in vec2 TexCoord;
+in vec3 Normal;
 out vec4 FragColor;
+uniform sampler2D u_texture_0;
+uniform vec3 u_view_pos;
+
+float stylize(float f, float segments)
+{
+    return max(0.16, floor(f * segments + 0.3) / segments);
+}
+
 void main() 
 {
+  vec4 diffuse = texture(u_texture_0, TexCoord);
   vec3 lightPos = vec3(16.0, 23.0, 0.0);
   
-  float lightning = pow(1.0 - length(lightPos - FragPos) / 16.0, 4.0);
+  vec3 lightDir = normalize(vec3(0.1,1,0.1));
 
-  FragColor = vec4(vec3(1.0, 0.0, 0.0) + vec3(lightning) * 1.5, 1.0);
+  float diff = max(dot(lightDir, Normal.xyz), 0.0);
+
+  float rim = 1.0 - max(dot(u_view_pos, Normal.xyz), 0.0);
+  rim = smoothstep(0.0, 1.0, rim);
+
+  FragColor = vec4(diffuse.rgb * stylize(diff, 2.0) + diffuse.rgb * stylize(rim * 0.5, 2.0), 1.0);
+  FragColor.rgb = pow(FragColor.rgb, vec3(1.0 / 2.2));
 }
 `;
 
@@ -110,12 +139,15 @@ window.onload = () => {
   precision highp float;
   layout (location=0) in vec3 aPos;
   layout (location=1) in vec3 aColor;
+  uniform mat4 u_projection;
+  uniform mat4 u_view;
+  uniform mat4 u_model;
   out vec3 FragPos;
   out vec3 Color;
   void main() {
-    FragPos = aPos.xyx;
+    FragPos = vec3(u_model * vec4(aPos.xyx, 1.0));
     Color = aColor;
-    gl_Position = vec4(aPos, 1.0);
+    gl_Position = u_projection * u_view * u_model * vec4(aPos, 1.0);
   }
   `;
 
@@ -143,8 +175,19 @@ window.onload = () => {
     }
     globalThis.gl = glContext;
   }
+  let projection = glm.perspective(glm.radians(45.0), 4.0 / 3.0, 0.1, 100.0);
+
   const shaderProgram = new ShaderProgram(vsSource, fsSource);
+  shaderProgram.use();
+  shaderProgram.setUniformMatrix4f("u_projection", projection.array);
+
   const cubeShader = new ShaderProgram(cubeVert, cubeFrag);
+  cubeShader.use();
+  cubeShader.setUniformMatrix4f("u_projection", projection.array);
+  cubeShader.setUniform1i("u_texture_0", 0);
+
+  const rabbitDiffuse = new ImageTexture("assets/rabbit.png");
+  const dirtDiffuse = new ImageTexture("assets/dirt.png");
 
   const vertexArray = new VertexArray([3, 3], [
     -1.0, -1.0, 0.0, 1.0, 0.0, 0.0,
@@ -154,7 +197,11 @@ window.onload = () => {
     [0, 2, 1,
       0, 3, 2]);
 
-  const cubeVAO = new VertexArray([3], cubePos, cubeIndices);
+  const planeVAO = new Asset_plane();
+
+  //const cubeVAO = new VertexArray([3], cubePos, cubeIndices);
+
+  const rabbitVAO = new Asset_rabbit();
 
   let lastTime = 0;
 
@@ -165,6 +212,12 @@ window.onload = () => {
     let dt = time - lastTime;
 
     //console.log(dt);
+    let cameraAngle = (time * 0.3) % Math.PI * 2;
+    let camX = Math.sin(cameraAngle) * 32.0;
+    let camZ = Math.cos(cameraAngle) * 32.0;
+    let viewPos = glm.vec3(camX, 32.0, camZ);
+    let view = glm.lookAt(viewPos, glm.vec3(0.0, 0.0, 0.0), glm.vec3(0.0, 1.0, 0.0));
+    let model;
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clearDepth(1.0); // Clear everything
@@ -172,24 +225,31 @@ window.onload = () => {
     gl.depthFunc(gl.LEQUAL); // Near things obscure far things
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    shaderProgram.use();
+    /*shaderProgram.use();
+    shaderProgram.setUniformMatrix4f("u_model", model.array);
+    shaderProgram.setUniformMatrix4f("u_view", view.array);
     shaderProgram.setUniform1f('u_time', time);
-    //vertexArray.draw();
+    vertexArray.draw();*/
 
     cubeShader.use();
 
-    let model = glm.mat4(1.0);
-    model = glm.scale(model, glm.vec3(1.0));
-    model = glm.rotate(model, time % 3.14, glm.vec3(0.0, 1.0, 0.0));
+    
 
-    let projection = glm.perspective(glm.radians(45.0), 4.0 / 3.0, 0.1, 100.0);
-
-    let view = glm.lookAt(glm.vec3(4.0, 4.0, 2.0), glm.vec3(0.0, 0.0, 0.0), glm.vec3(0.0, 1.0, 0.0));
-
-    cubeShader.setUniformMatrix4f("u_model", model.array);
-    cubeShader.setUniformMatrix4f("u_projection", projection.array);
     cubeShader.setUniformMatrix4f("u_view", view.array);
-    cubeVAO.draw();
+    cubeShader.setUniform3f("u_view_pos", viewPos.array);
+    model = glm.mat4(1.0);
+    model = glm.translate(model, glm.vec3(0.0, 0.0, 0.0));
+    //model = glm.rotate(model, Math.PI * 0.5, glm.vec3(1.0, 0.0, 0.0));
+    model = glm.scale(model, glm.vec3(100.0));
+    cubeShader.setUniformMatrix4f("u_model", model.array);
+    dirtDiffuse.bind();
+    planeVAO.draw();
+    model = glm.mat4(1.0);
+    model = glm.scale(model, glm.vec3(1.0));
+    //model = glm.rotate(model, time % Math.PI * 2, glm.vec3(0.0, 1.0, 0.0));
+    cubeShader.setUniformMatrix4f("u_model", model.array);
+    rabbitDiffuse.bind();
+    rabbitVAO.draw();
     requestAnimationFrame(drawScene);
     lastTime = time;
   };
